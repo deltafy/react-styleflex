5afc2ffd71e795f502b2f13b1434310d1b644138
starkfire <plpantoja49@gmail.com>
2024-02-03 17:18:07 +0800

    feat: define babel plugins, add optimizeDeps option

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import macros from 'vite-plugin-babel-macros'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    optimizeDeps: {
+        esbuildOptions: {
+            target: 'es2020'
+        }
+    },
     plugins: [
-        react()
+        react({
+            babel: {
+                plugins: [
+                    'babel-plugin-macros',
+                    'babel-plugin-styled-components'
+                ]
+            }
+        }),
+        macros()
     ],
 })
