0d76ef83ec5e1f67589b1a712dbcd58c3df44e70
starkfire <plpantoja49@gmail.com>
2024-02-03 19:54:11 +0800

    feat: working example of styled-components without twin.macro

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
 import StyledComponentsDemo from './components/StyledComponentsDemo'
+import StyledTwinMacroDemo from './components/StyledTwinMacroDemo'
 
+// you can also use styled-components without twin.macro
+import { styled } from 'styled-components'
+
+const DemoTitle = styled.p`
+    font-size: 1.125rem;
+    font-weight: 700;
+    margin: 1.25rem 0;
+`
+
+// external CSS also works 
 import './App.css'
 
 function App() {
-    const [count, setCount] = useState(0)
-
     return (
         <>
-            <StyledComponentsDemo />
-            <div className="flex items-center justify-center">
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
+            <div className="my-12">
+                <p className="text-lg font-bold my-5">Styled Components + twin.macro</p>
+                <StyledTwinMacroDemo />
             </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
+            <div className="my-12">
+                <DemoTitle>Styled Components only</DemoTitle>
+                <StyledComponentsDemo />
             </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
         </>
     )
 }
